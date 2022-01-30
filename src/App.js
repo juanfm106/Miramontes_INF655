4cd776faa142afb48294cf4f12bbfeed1d59014e
Juan <juanfm106@yahoo.com>
2022-01-29 21:46:20 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
 
 function App() {
+  const toDoList = [
+    {
+      id: 1,
+      title: "Laundry",
+      description: "Put clothes in washer",
+    },
+    {
+      id: 2,
+      title: "Clean Bathroom",
+      description: "Clean the shower and toilets",
+    },
+    {
+      id: 3,
+      title: "Wash car",
+      description: "Clean the car",
+    },
+  ];
+  const myName = "Juan";
+
+  const taskManager = () => {
+    const int = Math.floor(Math.random() * 3);
+    return toDoList[int].title;
+  };
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is my first react app</h1>
+      <p>
+        Hello {myName} you have a task of <h3>{taskManager()}</h3>
+      </p>
     </div>
   );
 }
 
 export default App;
+
